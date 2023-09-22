535a61d6acf37937e1e88e004eae22a48e5ae21e
Omar Aljoundi <omaraljoundi@gmail.com>
2023-09-23 01:11:58 +0300

    add logs

--- middleware.ts
+++ middleware.ts
@@ -9,7 +9,7 @@ export const config = {
      * 3. /_static (inside /public)
      * 4. all root files inside /public (e.g. /favicon.ico)
      */
-    "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
   ],
 };
 
@@ -17,16 +17,20 @@ export default async function middleware(req: NextRequest) {
   const url = req.nextUrl;
 
   // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
+
+  console.log("hostname", req.headers);
   const hostname = req.headers
     .get("host")!
     .replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
 
   const path = url.pathname;
+  console.log("path", path);
   if (
     hostname === "localhost:3000" ||
     hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN
   ) {
-    return NextResponse.rewrite(new URL(`${path}`, req.url));
+    console.log("OMAR");
+    return NextResponse.next();
   }
 
   // rewrite everything else to `/[domain]/[path] dynamic route
